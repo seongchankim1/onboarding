5d5ff458bb0d6ef0fc3dbc05e96005ff8faa931d
seongchankim1 <167050690+seongchankim1@users.noreply.github.com>
2024-11-08 01:26:47 +0900

    feature: 회원가입 추가

--- src/main/java/com/seongchan/onboarding/service/UserService.java
+++ src/main/java/com/seongchan/onboarding/service/UserService.java
@@ -4,6 +4,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.seongchan.onboarding.dto.SignupRequestDto;
+import com.seongchan.onboarding.entity.User;
+import com.seongchan.onboarding.entity.UserRole;
+import com.seongchan.onboarding.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,12 +15,21 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
 	private final PasswordEncoder passwordEncoder;
+	private final UserRepository userRepository;
 
 	public String signup(SignupRequestDto requestDto) {
 		String username = requestDto.getUsername();
 		String password = passwordEncoder.encode(requestDto.getPassword());
-		System.out.println(password);
+		String name = requestDto.getName();
 
-		return "hi";
+		User user = User.builder().
+			username(username).
+			password(password).
+			name(name).
+			userRole(UserRole.User).
+			build();
+		userRepository.save(user);
+
+		return username;
 	}
 }
