cb51410fd218ae7b43505b203192a68afdf75a36
seongchankim1 <167050690+seongchankim1@users.noreply.github.com>
2024-11-08 13:41:03 +0900

    feature: redis 추가

---
+++ src/main/java/com/seongchan/onboarding/common/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.seongchan.onboarding.common;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    public String host;
+
+    @Value("${redis.port}")
+    public int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
--- src/main/java/com/seongchan/onboarding/entity/UserRole.java
+++ src/main/java/com/seongchan/onboarding/entity/UserRole.java
@@ -1,7 +1,9 @@
 package com.seongchan.onboarding.entity;
 
 import jakarta.persistence.Enumerated;
+import lombok.Getter;
 
+@Getter
 public enum UserRole {
 
 	Admin,
